c662c7369bec47c459f53a62e42a7e41ba39dab5
Adelopoanas <aa.adelopoa@gmail.com>
2020-12-29 19:47:43 +0100

    Auto Commit

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
+const form = document.querySelector('.form');
+const username = document.querySelector('.username');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const confirmPassword = document.querySelector('confirm-password');
 
-  
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    checkValues();
+})
+
+
+function checkValues() {
+    const usernameValue = username.Value.trim();
+    const emailValue = email.Value.trim();
+    const passwordValue = password.Value.trim();
+    const confirmPasswordValue = confirm-password.Value.trim();
+
+
+    if (usernameValue === "") {
+        setErrorFor(username,'Usernmae cannot be blank')
+    }else{
+        setSuccessFor(username)
+    }
+}
+function setErrorFor(input,message) {
+    const formControl = input.parentElement;
+    const small = form.querySelector('small');
+
+    small.innerHTML = message;
+
+    formControl.className = 'form-control error'
+}
